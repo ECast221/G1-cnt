8857b488b1cbc03deee8ce66ffbbf1806705dfce
Manny Castaneda <Ecast221@gmail.com>
2020-07-19 18:18:58 -0400

    '1'

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from sys import stdout
 from image_inverter import ImageInverter
 import logging
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify, redirect, session, url_for
 from flask_socketio import SocketIO
 from camera import Camera
-
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(stdout))
@@ -12,6 +12,39 @@ app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
 camera = Camera(ImageInverter())
+oauth = OAuth(app)
+
+
+google = oauth.register(
+    name='google',
+    client_id='405875549769-pij6u473gq990958i1bdj6asi2mia3ms.apps.googleusercontent.com',
+    client_secret='ojGoh0PtLmvDJ14k_pAsvjZj',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
+
+
+@app.route('/')
+def login():
+    google = oauth.create_client('google')
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    # do something with the token and profile
+    session['email'] = user_info['email']
+    return redirect('/home')
 
 
 @socketio.on('input image', namespace='/test')
@@ -45,6 +78,20 @@ def gen():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/home')
+def home():
+    """Video streaming home page."""
+    email = dict(session).get('email', None)
+    return render_template('index.html')
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
 
 
 if __name__ == '__main__':
